353838f4d342cd04be41a37bf7b0e3af9fe8194f
Nichole Henry <nichole.henry12@gmail.com>
2019-06-05 13:17:37 -0500

    Add project files.

---
+++ GoodOleStLouie/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GoodOleStLouie.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
