b62a67677124ff6663861e9bfea6f3a031cebc4e
Nichole Henry <nichole.henry12@gmail.com>
2019-06-15 01:27:21 -0500

    commit

---
+++ GoodOleStLouie/Data/ILocationRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoodOleStLouie.Data
+{
+    interface ILocationRepository
+    {
+        List<Location> GetLocation();
+        int Save(Location location);
+        void Update(Location location);
+        void Delete(int id);
+    }
+}
---
+++ GoodOleStLouie/Data/LocationRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoodOleStLouie.Data
+{
+    public class LocationRepository : ILocationRepository
+    {
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Location> GetLocation()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Save(Location location)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Location location)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
