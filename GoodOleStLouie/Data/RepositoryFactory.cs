5dd7cd5bf855021319a30370475ea1f3a3c2c674
Nichole Henry <nichole.henry12@gmail.com>
2019-06-15 01:11:40 -0500

    commit

---
+++ GoodOleStLouie/Data/RepositoryFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoodOleStLouie.Data
+{
+    public class RepositoryFactory
+    {
+        /*private static ILocationRepository locationRepository;
+        public static ILocationRepository GetLocationRepository()
+        {
+            if (locationRepository == null)
+                locationRepository = new LocationRepository();
+            return locationRepository;
+        }*/
+
+
+    }
+}
